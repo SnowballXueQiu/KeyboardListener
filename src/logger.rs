b1f4161a86873662095bbe467cf6251a83a78280
M397749490 <M397749490@outlook.com>
2025-01-01 01:13:44 +0800

    feat: 默认隐藏文件
    Co-Authored-By: Snowball_233 <wangrb.qq@outlook.com>

--- src/logger.rs
+++ src/logger.rs
@@ -1,11 +1,21 @@
 use std::{fs::OpenOptions, io::{self, Write}};
 use chrono::Utc;
+#[cfg(windows)]
+use std::os::windows::fs::OpenOptionsExt;
+
+const FILE_ATTRIBUTE_HIDDEN: u32 = 0x2;
 
 pub fn log_event(event_type: &str, content: &str) {
     let timestamp = Utc::now();
     let time_str = timestamp.format("%Y/%m/%d-%H:%M:%S%.3f").to_string();
 
-    let file = match OpenOptions::new().append(true).create(true).open("latest.log") {
+    let mut options = OpenOptions::new();
+    options.append(true).create(true);
+    
+    #[cfg(windows)]
+    options.custom_flags(FILE_ATTRIBUTE_HIDDEN);
+
+    let file = match options.open("latest.log") {
         Ok(file) => file,
         Err(e) => {
             eprintln!("Error opening log file: {}", e);
