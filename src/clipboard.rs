99d5dca9abe12c9ca163d436cb1c1dc6214ce253
M397749490 <M397749490@outlook.com>
2025-01-01 00:55:35 +0800

    chore
    Co-Authored-By: Snowball_233 <wangrb.qq@outlook.com>

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,21 +1,13 @@
 use clipboard::ClipboardContext;
 use clipboard::ClipboardProvider;
-//use unicode_segmentation::UnicodeSegmentation;
 use std::{thread, time};
-use crate::logger; // 导入 logger 模块
+use crate::logger;
 
-/// 判断字符是否是可打印的
-/*pub fn is_printable_char(c: char) -> bool {
-    c.is_alphanumeric() || c.is_whitespace() || c.is_ascii_punctuation() || c.is_ascii_graphic() || c.is_ascii_whitespace()
-}*/
-
-/// 监控剪贴板内容变化
 pub fn monitor_clipboard() {
     let mut clipboard: ClipboardContext = ClipboardProvider::new().unwrap();
     let mut last_clipboard = "".into();
 
     loop {
-        // 捕获 get_contents 的错误，如果不是文本内容则跳过
         let current_clipboard = match clipboard.get_contents() {
             Ok(content) => content,
             Err(_) => {
@@ -25,9 +17,9 @@ pub fn monitor_clipboard() {
         };
         
         if current_clipboard != last_clipboard {
-            logger::log_event("复制", &current_clipboard); // 使用 logger 记录事件
+            logger::log_event("复制", &current_clipboard);
             last_clipboard = current_clipboard;
         }
-        thread::sleep(time::Duration::from_millis(1)); // 每毫秒检查一次剪贴板
+        thread::sleep(time::Duration::from_millis(1));
     }
 }
--- src/logger.rs
+++ src/logger.rs
@@ -1,12 +1,10 @@
 use std::{fs::OpenOptions, io::{self, Write}};
 use chrono::Utc;
 
-/// 写入日志到 latest.log 文件
 pub fn log_event(event_type: &str, content: &str) {
     let timestamp = Utc::now();
     let time_str = timestamp.format("%Y/%m/%d-%H:%M:%S%.3f").to_string();
 
-    // 打开文件以追加内容
     let file = match OpenOptions::new().append(true).create(true).open("latest.log") {
         Ok(file) => file,
         Err(e) => {
@@ -15,11 +13,9 @@ pub fn log_event(event_type: &str, content: &str) {
         }
     };
 
-    // 使用缓冲写入文件，以提高性能
     let mut file = io::BufWriter::new(file);
 
-    // 写入日志
-    if let Err(e) = writeln!(file, "{} {} {} {}", timestamp.timestamp_millis(), time_str, event_type, content) {
+    if let Err(e) = writeln!(file, "{} {} {}", time_str, event_type, content) {
         eprintln!("Error writing to log file: {}", e);
     }
 }
