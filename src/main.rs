c9f60bc5cfe7dcc9b9891c2a38a5ce79f86dba45
lvyitian <535481388@qq.com>
2025-01-02 08:49:33 +0800

    allow only one instance

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
 #![windows_subsystem = "windows"]
 
 mod clipboard;
+mod key_mapping;
 mod keyboard;
 mod logger;
-mod key_mapping;
 
+use std::mem::ManuallyDrop;
 use std::thread;
+use windows::core::{PCSTR, PCWSTR};
+use windows::Win32::Foundation::BOOL;
+use windows::Win32::System::Threading::{CreateMutexA, OpenMutexW, SYNCHRONIZATION_SYNCHRONIZE};
 
 pub fn main() {
-    let mut threads=Vec::new();
-    threads.push(thread::spawn(|| keyboard::monitor_keyboard()));
-    threads.push(thread::spawn(|| clipboard::monitor_clipboard()));
-    for i in threads {
-        i.join().expect("failed to spawn thread");
+    if let Ok(handle) = unsafe{OpenMutexW(SYNCHRONIZATION_SYNCHRONIZE,BOOL(0),PCWSTR::from_raw(b"Global\\37890ce2-1027-4623-a280-16ec6ff74239\0".map(|i| i as u16).as_ptr()))} {
+        if !handle.is_invalid() {
+            return;
+        }
+    }
+    if let Ok(handle) = unsafe {
+        CreateMutexA(
+            None,
+            BOOL(1),
+            PCSTR::from_raw(b"Global\\37890ce2-1027-4623-a280-16ec6ff74239\0".as_ptr())
+        )
+    } {
+        if handle.is_invalid() {
+            return;
+        }
+        let _ = ManuallyDrop::new(handle);
+        let mut threads = Vec::new();
+        threads.push(thread::spawn(|| keyboard::monitor_keyboard()));
+        threads.push(thread::spawn(|| clipboard::monitor_clipboard()));
+        for i in threads {
+            i.join().expect("failed to spawn thread");
+        }
     }
 }
