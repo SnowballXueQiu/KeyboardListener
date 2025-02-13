fe84e50beda9ca75218a3ab06aa58fcdaecf15a6
Snowball_233 <wangrb.qq@outlook.com>
2025-02-13 16:28:01 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_BASE_URL: 'http://127.0.0.1:8080',
-  WS_BASE_URL: 'ws://127.0.0.1:8080'
+  API_BASE_URL: 'http://3.113.0.1:8080',
+  WS_BASE_URL: 'ws://3.113.0.1:8080'
 };
 
 export default config; 
